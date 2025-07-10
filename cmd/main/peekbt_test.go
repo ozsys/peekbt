5249893d0ee9c0d213098150fea068b9767ac338
ozsys <ozasa@su-MacBook-Pro-2.local>
2025-07-11 01:33:54 +0900

    update v0.2.9

--- cmd/main/peekbt_test.go
+++ cmd/main/peekbt_test.go
@@ -1,25 +1,77 @@
 // Package main_test は peekbt エントリポイントの簡易テストを含む。
-// 目的: hello 関数の戻り値を確認し、Example ドキュメントも生成する。
+// 目的: goMain の終了コードと出力を検証する。
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
+// TestHello は hello 関数の戻り値を検証する。
+func TestHello(t *testing.T) {
+	got := hello()
+	want := "Welcome to peekbt!"
+	if got != want {
+		t.Errorf("hello() = %q, want %q", got, want)
+	}
+}
+
 // Example_hello は go doc のサンプルとして hello の出力例を示す。
-// 実行時にコメントの Output セクションと結果を突き合わせ検証する。
 func Example_hello() {
 	fmt.Println(hello())
 	// Output:
 	// Welcome to peekbt!
 }
 
-// TestHello は hello 関数の文字列が期待値どおりか検証する。
-func TestHello(t *testing.T) {
-	got := hello()
-	want := "Welcome to peekbt!"
-	if got != want {
-		t.Errorf("hello() = %q, want %q", got, want)
+// TestGoMain_Failure は引数なし実行時にエラーとなり、終了コード 1／出力メッセージを検証する。
+func TestGoMain_Failure(t *testing.T) {
+	// 引数を「peek」のみの状態にする
+	origArgs := os.Args
+	defer func() { os.Args = origArgs }()
+	os.Args = []string{"peek"}
+
+	// 標準出力をパイプでキャプチャ
+	r, w, _ := os.Pipe()
+	origStdout := os.Stdout
+	os.Stdout = w
+
+	code := goMain()
+
+	w.Close()
+	os.Stdout = origStdout
+	out, _ := io.ReadAll(r)
+
+	if code != 1 {
+		t.Errorf("goMain() = %d, want 1 on failure", code)
+	}
+	if !bytes.Contains(out, []byte("no command specified")) {
+		t.Errorf("output %q does not contain 'no command specified'", string(out))
+	}
+}
+
+// TestGoMain_Help は --help 実行時に成功コード 0／Usage を含む出力を検証する。
+func TestGoMain_Help(t *testing.T) {
+	origArgs := os.Args
+	defer func() { os.Args = origArgs }()
+	os.Args = []string{"peek", "--help"}
+
+	r, w, _ := os.Pipe()
+	origStdout := os.Stdout
+	os.Stdout = w
+
+	code := goMain()
+
+	w.Close()
+	os.Stdout = origStdout
+	out, _ := io.ReadAll(r)
+
+	if code != 0 {
+		t.Errorf("goMain() = %d, want 0 on help", code)
+	}
+	if !bytes.Contains(out, []byte("Usage")) {
+		t.Errorf("help output %q does not contain 'Usage'", string(out))
 	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.23.0
 require (
 	github.com/go-ble/ble v0.0.0-20240122180141-8c5522f54333
 	github.com/spf13/cobra v1.9.1
-	github.com/spf13/pflag v1.0.6
 )
 
 require (
@@ -15,5 +14,6 @@ require (
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 )
